632d6f45fcb9ded36551eed167d7eae35b6d06ec
Alen Alduk <youstylez@gmail.com>
2023-09-22 23:44:39 +0200

    working on tooltip

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,6 +8,6 @@ import { TooltipPosition } from '@angular/material/tooltip';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-  positionOptions: TooltipPosition[] = ['after', 'before', 'above', 'below', 'left', 'right'];
+  positionOptions: TooltipPosition[] = ['above', 'after', 'before', 'below', 'left', 'right'];
   position = new FormControl(this.positionOptions[0]);
 }
