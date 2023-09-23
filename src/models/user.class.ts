b60095dc61a721acb8a4a664cec66647c5ec0a88
Alen Alduk <youstylez@gmail.com>
2023-09-23 17:56:36 +0200

    added email

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,6 +1,7 @@
 export class User {
     firstName: string;
     lastName: string;
+    email: string;
     birthDate: number;
     street: string;
     zipCode: number;
@@ -11,6 +12,7 @@ constructor(obj?: any){
   
         this.firstName = obj ? obj.firstName : '';
         this.lastName = obj ? obj.lastName : '';
+        this.email = obj ? obj.email: '';
         this.birthDate = obj ? obj.birthDate : '';
         this.street = obj ? obj.street : '';
         this.zipCode = obj ? obj.zipCode : '';
@@ -22,6 +24,7 @@ public toJson(){
     return {
         firstName:  this.firstName,
         lastName: this.lastName,
+        email: this.email,
         birthDate: this.birthDate,
         street:  this.street,
         zipCode:  this.zipCode,
